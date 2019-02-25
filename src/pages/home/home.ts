409607ec6e0b93c222da344fb23d09006fd3aa31
Sean McGillis <slcidevice@yahoo.com>
2019-02-25 01:28:07 -0700

    created a home page to display the workout information for each day

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public storage: Storage) {
 
+  }
+
+  dataGetTest() {
+    this.storage.get('name').then((data) => {
+      console.log('name', data); //@DEBUG
+    })
   }
 
 }
