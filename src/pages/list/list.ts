44f6dbeb00ca3dc4d9f140826f20b3ccd458f6db
Sean McGillis <slcidevice@yahoo.com>
2019-02-25 01:29:35 -0700

    used the existing list page to start implementing data storage related to the amount of weight the user will be starting with for each exercise

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,37 +1,107 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { callLifecycleHooksChildrenFirst } from '@angular/core/src/view/provider';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+  public exercises = ["bench","squat","shoulderPress","deadLift"];
+  public bench = {};
+  public squat = {};
+  public shoulderPress = {};
+  public deadLift = {};
+  public cycle : FormGroup;
+  public test = false;
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public storage: Storage,
+    public formBuilder: FormBuilder
+    ) { 
+      
 
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      this.storage.get('ORMBench').then((data) =>{
+        this.cycle.value.bench = data;
       });
+      this.storage.get('ORMSquat').then((data) =>{
+        this.cycle.value.squat = data;
+      });
+      this.storage.get('ORMShoulderPress').then((data) =>{
+        this.cycle.value.shoulderPress = data;
+      });
+      this.storage.get('ORMDeadLift').then((data) =>{
+        this.cycle.value.deadLift = data;
+      });
+      
+
+      this.cycle = this.formBuilder.group({
+        bench: ['',],
+        squat: [''],
+        shoulderPress: [''],
+        deadLift: ['']
+      });
+      
     }
+
+  testValue() {
+    this.test = !this.test;
+  }
+  
+  setORM() {
+    this.storage.set('ORMBench', this.cycle.value.bench)
+    this.storage.set('ORMSquat', this.cycle.value.squat)
+    this.storage.set('ORMShoulderPress', this.cycle.value.shoulderPress)
+    this.storage.set('ORMDeadLift', this.cycle.value.deadLift)
+    console.log('formData', this.cycle.value); //@DEBUG
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
+  showORM() {
+    this.storage.get('ORMBench').then((data) => {
+      console.log('ORM Bench', data); //@DEBUG
+    });
+    this.storage.get('ORMSquat').then((data) => {
+      console.log('ORM Squat', data); //@DEBUG
+    });
+    this.storage.get('ORMShoulderPress').then((data) => {
+      console.log('ORM ShoulderPress', data); //@DEBUG
+    });
+    this.storage.get('ORMDeadLift').then((data) => {
+      console.log('ORM DeadLift', data); //@DEBUG
     });
   }
+
+  clearORM() {
+    this.storage.remove('ORMBench').then((data) => {
+      console.log('ORM Bench', data); //@DEBUG
+    });
+    this.storage.remove('ORMSquat').then((data) => {
+      console.log('ORM Squat', data); //@DEBUG
+    });
+    this.storage.remove('ORMShoulderPress').then((data) => {
+      console.log('ORM ShoulderPress', data); //@DEBUG
+    });
+    this.storage.remove('ORMDeadLift').then((data) => {
+      console.log('ORM DeadLift', data); //@DEBUG
+    });
+  }
+
+
+  dataSetTest() {
+    this.storage.set('name', 'sean').then((successData) => {
+      console.log('stored',successData ); //@DEBUG
+    });
+  }
+
+  dataGetTest() {
+    this.storage.get('name').then((data) => {
+      console.log('name', data); //@DEBUG
+    })
+  }
+  
 }
