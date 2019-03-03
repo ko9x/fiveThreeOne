842b3733d1389aa884854cd776d104cc9fc80803
Sean McGillis <slcidevice@yahoo.com>
2019-03-03 16:06:54 -0700

    added all the functions to calculate the weight for each exercise and round it to the nearest 2.5

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,16 +8,17 @@ import { Storage } from '@ionic/storage';
 })
 export class HomePage {
 
-  public ORMBench
-  public ORMSquat
-  public ORMShoulderPress
-  public ORMDeadLift
-  public week1 = {}
-  public set1: string;
+  public ORMBench;
+  public ORMSquat;
+  public ORMShoulderPress;
+  public ORMDeadLift;
   public Bench = 'hello';
   public days: string;
   public exercises = ["Bench","Squat","ShoulderPress","DeadLift"];
-  public cycle = [{
+  public week1Percents = [65, 75, 85];
+  public week2Percents = [70, 80, 90];
+  public week3Percents = [75, 85, 95];
+  public cycle: Array<any> = [{
     week1: [
       {set1: [{Bench: "" },{Squat: ""},{ShoulderPress: ""},{DeadLift: ""}]},
       {set2: [{Bench: "" },{Squat: ""},{ShoulderPress: ""},{DeadLift: ""}]},
@@ -56,15 +57,77 @@ export class HomePage {
   }
 
   runIt(){
+    console.log('week 1---------'); //@DEBUG
     for (let ex of this.exercises) {
       let orm = "ORM" + ex
-      this.cycle[0].week1[0].set1[ex] = (65/100) * (90/100 * Number(this[orm]))
-      console.log('ex', this.cycle[0].week1[0].set1[ex]); //@DEBUG
-      console.log('this with []', [this]); //@DEBUG
-      console.log('this without []', this); //@DEBUG
-      console.log('thing',[this][0][orm] ); //@DEBUG
-      // console.log('ormbench', this.ORMBench); //@DEBUG
+      this.cycle[0].week1[0].set1[ex] = Math.round((65/100) * (90/100 * Number(this[orm]))/2.5) * 2.5;
+      console.log('week1 set1', this.cycle[0].week1[0].set1[ex]); //@DEBUG
     }
+    for (let ex of this.exercises) {
+      let orm = "ORM" + ex
+      this.cycle[0].week1[1].set2[ex] = Math.round((75/100) * (90/100 * Number(this[orm]))/2.5) * 2.5;
+      console.log('week1 set2', this.cycle[0].week1[1].set2[ex]); //@DEBUG
+    }
+    for (let ex of this.exercises) {
+      let orm = "ORM" + ex
+      this.cycle[0].week1[2].set3[ex] = Math.round((85/100) * (90/100 * Number(this[orm]))/2.5) * 2.5;
+      console.log('week1 set3', this.cycle[0].week1[2].set3[ex]); //@DEBUG
+    }
+    console.log('week 2----------'); //@DEBUG
+    for (let ex of this.exercises) {
+      let orm = "ORM" + ex
+      this.cycle[0].week2[0].set1[ex] = Math.round((70/100) * (90/100 * Number(this[orm]))/2.5) * 2.5;
+      console.log('week2 set1', this.cycle[0].week2[0].set1[ex]); //@DEBUG
+    }
+    for (let ex of this.exercises) {
+      let orm = "ORM" + ex
+      this.cycle[0].week2[1].set2[ex] = Math.round((80/100) * (90/100 * Number(this[orm]))/2.5) * 2.5;
+      console.log('week2 set2', this.cycle[0].week2[1].set2[ex]); //@DEBUG
+    }
+    for (let ex of this.exercises) {
+      let orm = "ORM" + ex
+      this.cycle[0].week2[2].set3[ex] = Math.round((90/100) * (90/100 * Number(this[orm]))/2.5) * 2.5;
+      console.log('week2 set3', this.cycle[0].week2[2].set3[ex]); //@DEBUG
+    }
+    console.log('week 3--------------'); //@DEBUG
+    
+    for (let ex of this.exercises) {
+      let orm = "ORM" + ex
+      this.cycle[0].week3[0].set1[ex] = Math.round((75/100) * (90/100 * Number(this[orm]))/2.5) * 2.5;
+      console.log('week3 set1', this.cycle[0].week3[0].set1[ex]); //@DEBUG
+    }
+    for (let ex of this.exercises) {
+      let orm = "ORM" + ex
+      this.cycle[0].week3[1].set2[ex] = Math.round((85/100) * (90/100 * Number(this[orm]))/2.5) * 2.5;
+      console.log('week3 set2', this.cycle[0].week3[1].set2[ex]); //@DEBUG
+    }
+    for (let ex of this.exercises) {
+      let orm = "ORM" + ex
+      let rewNum
+      this.cycle[0].week3[2].set3[ex] = Math.round((95/100) * (90/100 * Number(this[orm]))/2.5) * 2.5;
+      console.log('week3 set3', this.cycle[0].week3[2].set3[ex]); //@DEBUG
+      console.log('mathing',Math.round(this.cycle[0].week3[2].set3[ex]/2.5) * 2.5 ); //@DEBUG
+    }
+  }
+
+  tryIt() {
+    for(let week of this.cycle) {
+      for (let set of this.cycle[0].week1) {
+        for (let ex of this.exercises) {
+          this.cycle[0]
+        }
+        console.log('set', set); //@DEBUG
+      }
+      console.log('week', week); //@DEBUG
+    }
+  }
+
+  mathIt() {
+    console.log(Math.round(107.1/2.5) * 2.5 ); //@DEBUG
+  }
+
+  displayIt() {
+    console.log('display', this.cycle[0].week2); //@DEBUG
   }
 
 }
