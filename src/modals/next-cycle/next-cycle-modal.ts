4fc411217321bac065dcac7bf067b947d993ea65
Sean McGillis <slcidevice@yahoo.com>
2019-03-11 18:36:38 -0600

    created a currentCycle property and a function to get it from local storage

--- src/modals/next-cycle/next-cycle-modal.ts
+++ src/modals/next-cycle/next-cycle-modal.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { ViewController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
 
 @Component({
     selector: 'next-cycle-modal',
     templateUrl: 'next-cycle-modal.html'
 })
 export class NextCycleModal {
+
+    public currentCycle: string;
     
+    constructor(public storage: Storage) {
         
+        this.storage.get('currentCycle').then((data) => {
+            this.currentCycle = data || 'Cycle 1';
+        });
+    }
       
 }
