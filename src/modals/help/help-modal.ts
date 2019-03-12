c048d78964dda0f18735cbfeb339afa698905d4a
Sean McGillis <slcidevice@yahoo.com>
2019-03-11 21:07:13 -0600

    filled out the help modal

--- src/modals/help/help-modal.ts
+++ src/modals/help/help-modal.ts
@@ -10,6 +10,12 @@ import { Storage } from '@ionic/storage';
 export class HelpModal {
 
     public currentCycle: string;
+    public exampleSets = [
+        {name: 'set 1', weight: "70%", reps: "1x5"},
+        {name: 'set 2', weight: "80%", reps: "1x5"},
+        {name: 'set 3', weight: "90%", reps: "1x5"},
+        {name: 'set 4', weight: "100%", reps: "1x3-5"}
+      ]
     
     constructor(
         public storage: Storage,
